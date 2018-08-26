aa2db09fc6cc55e23d6082e1a3e246d208beee82
susuxi <893136369@qq.com>
2018-08-27 00:37:30 +0800

    url爬虫--url管理

---
+++ spider.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 26 23:24:34 2018
+
+@author: susan
+"""
+
+import threading
+
+
+class main(object):
+    def _intit_(self,root,threadNum):
+        self.url
